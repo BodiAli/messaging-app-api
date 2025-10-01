5d616b74736b2e3d31cd5221aec2925f3097d126
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-10-02 01:54:38 +0300

    Feat: Initialize linter

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,18 @@
 import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
 import tseslint from "typescript-eslint";
 import globals from "globals";
+import vitest from "@vitest/eslint-plugin";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 import { globalIgnores } from "eslint/config";
 
-export default tseslint.config([
+export default defineConfig([
   globalIgnores(["dist"]),
   {
     files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommendedTypeChecked,
+      tseslint.configs.strictTypeChecked,
       tseslint.configs.stylisticTypeChecked,
     ],
     languageOptions: {
@@ -32,6 +34,15 @@ export default tseslint.config([
       ],
     },
   },
-
   eslintConfigPrettier,
+  {
+    files: ["**/*.test.{ts,tsx}"],
+    plugins: {
+      vitest,
+    },
+    rules: {
+      ...vitest.configs.all.rules,
+      "@typescript-eslint/unbound-method": "off",
+    },
+  },
 ]);
