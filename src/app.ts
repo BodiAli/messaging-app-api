205762662bd8b95d063fb4b75af7d4518ef65eae
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-10-05 23:45:36 +0300

    Feat: Initialize server

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
+import "dotenv/config";
 import express from "express";
+import authRouter from "./routes/authRouter.js";
 
 const app = express();
+
+app.use(express.json());
+
+app.use("/auth", authRouter);
+
+const PORT = process.env["PORT"] ?? 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
