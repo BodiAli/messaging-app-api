c0538a79a05aa2ad9bc782b2264050fd37784c9f
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-10-10 01:58:51 +0300

    Feat: Pass expiresIn as a param so that it can be passed a different argument depending on login method

--- src/lib/issueJwt.ts
+++ src/lib/issueJwt.ts
@@ -1,8 +1,9 @@
 import jwt from "jsonwebtoken";
+import type { StringValue } from "ms";
 
-export default function issueJwt(id: string) {
+export default function issueJwt(id: string, expiresIn: StringValue) {
   const token = jwt.sign({ sub: id }, process.env.SECRET_KEY, {
-    expiresIn: "2w",
+    expiresIn,
   });
 
   return token;
