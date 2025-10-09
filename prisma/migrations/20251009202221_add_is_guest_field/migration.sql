f9a442a705b004945e9b7d4e7dd637608cb284f7
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-10-09 23:24:17 +0300

    Feat: Add an isGuest field to allow users to log in as a guest

---
+++ prisma/migrations/20251009202221_add_is_guest_field/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - Made the column `type` on table `Notification` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- AlterTable
+ALTER TABLE "Notification" ALTER COLUMN "type" SET NOT NULL;
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "isGuest" BOOLEAN NOT NULL DEFAULT false;
