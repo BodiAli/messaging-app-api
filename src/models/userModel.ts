773913b5d3eb37ce811bd4f8afd2fc4f67a6d20f
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-10-10 01:59:36 +0300

    Feat: Create two queries to test hashing password and to create or find a guest record

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -28,6 +28,16 @@ export async function getUserRecordById(id: string) {
   return user;
 }
 
+export async function getUserRecordByIdUnsafe(id: string) {
+  const user = await prisma.user.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  return user;
+}
+
 export async function getUserRecordByUsername(username: string) {
   const user = await prisma.user.findUnique({
     where: {
@@ -36,4 +46,24 @@ export async function getUserRecordByUsername(username: string) {
   });
 
   return user;
+}
+
+export async function getOrCreateUserRecord(username: string, password: string) {
+  const guestUser = await prisma.user.upsert({
+    where: {
+      username,
+    },
+    update: {},
+    create: {
+      password,
+      username,
+      lastSeen: new Date(),
+      isGuest: true,
+    },
+    omit: {
+      password: true,
+    },
+  });
+
+  return guestUser;
 }
