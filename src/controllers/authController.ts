67975a33bb3876451c7b2e0c05fd03143c87f9ee
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-10-09 03:46:22 +0300

    Feat: Create logic to handle validation errors while creating new user && sending a JWT if validation succeeds

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,9 +1,28 @@
 import type { Request, Response } from "express";
-import { createUserRecord } from "../models/userModel.js";
+import { createUserRecord, getUserRecordByUsername } from "../models/userModel.js";
+import { UserSignUpSchema } from "../lib/zodSchemas.js";
+import issueJwt from "../lib/issueJwt.js";
 
-export async function createUser(req: Request, res: Response) {
-  const user = await createUserRecord();
-  console.log(user);
+export async function createUser(
+  req: Request<object, object, { username: string; password: string; confirmPassword: string }>,
+  res: Response
+) {
+  const result = UserSignUpSchema.safeParse(req.body);
 
-  res.json({ msg: "hi" });
+  if (!result.success) {
+    res.status(400).json({ errors: result.error.issues });
+    return;
+  }
+
+  const doesUserExist = !!(await getUserRecordByUsername(req.body.username));
+
+  if (doesUserExist) {
+    res.status(409).json({ errors: [{ message: "A user with this username already exists." }] });
+    return;
+  }
+
+  const createUser = await createUserRecord(req.body.username, req.body.confirmPassword);
+  const token = issueJwt(createUser.id);
+
+  res.json({ token });
 }
