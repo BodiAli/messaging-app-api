86bc9109b268ca2964ed6d6a2334df0dc8d7a6ca
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-10-09 03:49:28 +0300

    Feat: Implement tests to check for form submition errors

--- src/tests/authRouter.test.ts
+++ src/tests/authRouter.test.ts
@@ -6,29 +6,49 @@ import { createUserRecord } from "../models/userModel.js";
 
 const app = express();
 
+app.use(express.json());
+
 app.use("/auth", authRouter);
 
-app.use((err, req, res, next) => {
-  console.log("ERROR!!!", err);
-});
-
 describe("authRouter test", () => {
-  describe("authenticate requests to users", () => {
-    describe("given POST request to /auth/log-in", () => {
-      it("should return 400 status with validation errors", async () => {
+  describe("create user", () => {
+    describe("given POST request to /auth/sign-up", () => {
+      it("should return 400 status when username or password are not valid", async () => {
+        expect.hasAssertions();
+
+        const response = await request(app)
+          .post("/auth/sign-up")
+          .type("json")
+          .send({ username: "", password: "123", confirmPassword: "321" })
+          .expect("Content-type", /json/)
+          .expect(400);
+
+        const typedResponseBody = response.body as { errors: { message: string }[] };
+
+        expect(typedResponseBody.errors[0]?.message).toBe("Username cannot be empty.");
+        expect(typedResponseBody.errors[1]?.message).toBe("Password must be at least 5 characters.");
+        expect(typedResponseBody.errors[2]?.message).toBe("Passwords do not match.");
+      });
+
+      it("should return 409 status when username already exists", async () => {
         expect.hasAssertions();
 
         await createUserRecord("bodi", "12345");
 
         const response = await request(app)
-          .post("/auth/log-in")
-          .send({ username: " ", password: "1234" })
+          .post("/auth/sign-up")
+          .type("json")
+          .send({ username: "bodi", password: "54321", confirmPassword: "54321" })
           .expect("Content-type", /json/)
-          .expect(400);
+          .expect(409);
 
-        const responseBody = response.body as { errors: { msg: string }[] };
+        const typedResponseBody = response.body as { errors: { message: string }[] };
 
-        expect(responseBody.errors[0]?.msg).toBe("Username cannot be empty");
+        expect(typedResponseBody.errors[0]?.message).toBe("A user with this username already exists.");
+      });
+
+      it.todo("should return 200 status and return JWT token when form submission is valid", () => {
+        expect.hasAssertions();
       });
     });
   });
