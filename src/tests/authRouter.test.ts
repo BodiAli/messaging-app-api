b03576622c8829c5ee2644e475a9cb0ca498120b
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-10-10 01:57:40 +0300

    Feat: Create tests for log in happy path and for guest login && multiple fixes

--- src/tests/authRouter.test.ts
+++ src/tests/authRouter.test.ts
@@ -2,8 +2,15 @@ import express from "express";
 import request from "supertest";
 import { describe, expect, it } from "vitest";
 import { jwt } from "zod";
+import bcrypt from "bcrypt";
 import authRouter from "../routes/authRouter.js";
-import { createUserRecord, getUserRecordById } from "../models/userModel.js";
+import {
+  createUserRecord,
+  getOrCreateUserRecord,
+  getUserRecordById,
+  getUserRecordByIdUnsafe,
+  getUserRecordByUsername,
+} from "../models/userModel.js";
 import type { User } from "../generated/prisma/index.js";
 import "../config/passportConfig.js";
 
@@ -17,10 +24,15 @@ interface ResponseError {
   errors: { message: string }[];
 }
 
+interface ResponseSuccess {
+  token: string;
+  user: User;
+}
+
 describe("authRouter test", () => {
-  describe("create user", () => {
-    describe("given POST request to /auth/sign-up", () => {
-      it("should return 400 status when inputs are not valid", async () => {
+  describe("create user POST /auth/sign-up", () => {
+    describe("given invalid inputs", () => {
+      it("should return 400 status and error messages", async () => {
         expect.hasAssertions();
 
         const response = await request(app)
@@ -36,8 +48,10 @@ describe("authRouter test", () => {
         expect(typedResponseBody.errors[1]?.message).toBe("Password must be at least 5 characters.");
         expect(typedResponseBody.errors[2]?.message).toBe("Passwords do not match.");
       });
+    });
 
-      it("should return 409 status when username already exists", async () => {
+    describe("given username already exists", () => {
+      it("should return 409 status and error message", async () => {
         expect.hasAssertions();
 
         await createUserRecord("bodi", "12345");
@@ -53,8 +67,10 @@ describe("authRouter test", () => {
 
         expect(typedResponseBody.errors[0]?.message).toBe("A user with this username already exists.");
       });
+    });
 
-      it("should return 201 status and return JWT token and user when form submission is valid", async () => {
+    describe("given valid inputs", () => {
+      it("should return 201 status, JWT token and user object", async () => {
         expect.hasAssertions();
 
         const response = await request(app)
@@ -64,7 +80,9 @@ describe("authRouter test", () => {
           .expect("Content-type", /json/)
           .expect(201);
 
-        const typedResponseBody = response.body as { token: string; user: User };
+        const typedResponseBody = response.body as ResponseSuccess;
+
+        typedResponseBody.user.lastSeen = new Date(typedResponseBody.user.lastSeen);
 
         const isTokenValidJwt = jwt().safeParse(typedResponseBody.token).success;
         const createdUser = await getUserRecordById(typedResponseBody.user.id);
@@ -74,14 +92,38 @@ describe("authRouter test", () => {
         }
 
         expect(isTokenValidJwt).toBe(true);
-        expect(createdUser.username).toBe("bodi");
+        expect(typedResponseBody.user).toStrictEqual(createdUser);
+      });
+
+      it("should hash password when creating user record", async () => {
+        expect.hasAssertions();
+
+        const response = await request(app)
+          .post("/auth/sign-up")
+          .type("json")
+          .send({ username: "bodi", password: "12345", confirmPassword: "12345" })
+          .expect("Content-type", /json/)
+          .expect(201);
+
+        const typedResponseBody = response.body as ResponseSuccess;
+
+        const createdUser = await getUserRecordByIdUnsafe(typedResponseBody.user.id);
+
+        if (!createdUser) {
+          throw new Error("User not found");
+        }
+
+        const doesPasswordMatch = await bcrypt.compare("12345", createdUser.password);
+
+        expect(createdUser.password).not.toBe("12345");
+        expect(doesPasswordMatch).toBe(true);
       });
     });
   });
 
-  describe("authenticate user", () => {
-    describe("given POST request to /auth/log-in", () => {
-      it("should return 400 status when inputs are not valid", async () => {
+  describe("authenticate user POST /auth/log-in", () => {
+    describe("given invalid inputs", () => {
+      it("should return 400 status and error messages", async () => {
         expect.hasAssertions();
 
         const response = await request(app)
@@ -96,7 +138,9 @@ describe("authRouter test", () => {
         expect(typedResponseBody.errors[0]?.message).toBe("Username cannot be empty.");
         expect(typedResponseBody.errors[1]?.message).toBe("Password cannot be empty.");
       });
+    });
 
+    describe("given valid inputs but invalid credentials", () => {
       it("should return 401 status and an error message when credentials are invalid", async () => {
         expect.hasAssertions();
 
@@ -113,9 +157,82 @@ describe("authRouter test", () => {
 
         expect(typedResponseBody.errors[0]?.message).toBe("Incorrect username or password.");
       });
+    });
 
-      it.todo("should return 200 status, JWT, and user when credentials are valid", async () => {
+    describe("given valid inputs and credentials", () => {
+      it("should return 200 status, JWT, and user when credentials are valid", async () => {
         expect.hasAssertions();
+
+        const hashedPassword = await bcrypt.hash("12345", 10);
+        const existingUser = await createUserRecord("bodi", hashedPassword);
+
+        const response = await request(app)
+          .post("/auth/log-in")
+          .type("json")
+          .send({ username: "bodi", password: "12345" })
+          .expect("Content-type", /json/)
+          .expect(200);
+
+        const typedResponseBody = response.body as ResponseSuccess;
+
+        typedResponseBody.user.lastSeen = new Date(typedResponseBody.user.lastSeen);
+
+        const isTokenValidJwt = jwt().safeParse(typedResponseBody.token).success;
+
+        expect(isTokenValidJwt).toBe(true);
+        expect(typedResponseBody.user.isGuest).toBe(false);
+        expect(typedResponseBody.user).toStrictEqual(existingUser);
+      });
+    });
+  });
+
+  describe("log in as guest GET /auth/guest", () => {
+    describe("given guest record not found inside User model", () => {
+      it("should return 200 status, create a guest record and return it with JWT", async () => {
+        expect.hasAssertions();
+
+        const guestUser = await getUserRecordByUsername("guest-user");
+
+        expect(guestUser).toBeNull();
+
+        const response = await request(app).get("/auth/guest").expect("Content-type", /json/).expect(200);
+
+        const typedResponseBody = response.body as ResponseSuccess;
+
+        typedResponseBody.user.lastSeen = new Date(typedResponseBody.user.lastSeen);
+
+        const createdGuestUser = await getUserRecordByUsername("guest-user");
+
+        if (!createdGuestUser) {
+          throw new Error("Guest user record not found");
+        }
+
+        const { password: _password, ...createdGuestUserWithoutPassword } = createdGuestUser;
+
+        const isTokenValidJwt = jwt().safeParse(typedResponseBody.token).success;
+
+        expect(isTokenValidJwt).toBe(true);
+        expect(createdGuestUser.isGuest).toBe(true);
+        expect(typedResponseBody.user).toStrictEqual(createdGuestUserWithoutPassword);
+      });
+    });
+
+    describe("given an already existing guest record inside User model", () => {
+      it("should return 200 status and return it with JWT", async () => {
+        expect.hasAssertions();
+
+        const existingGuestUser = await getOrCreateUserRecord("guest-user", "guest password");
+
+        const response = await request(app).get("/auth/guest").expect("Content-type", /json/).expect(200);
+
+        const typedResponseBody = response.body as ResponseSuccess;
+        typedResponseBody.user.lastSeen = new Date(typedResponseBody.user.lastSeen);
+
+        const isTokenValidJwt = jwt().safeParse(typedResponseBody.token).success;
+
+        expect(isTokenValidJwt).toBe(true);
+        expect(typedResponseBody.user.isGuest).toBe(true);
+        expect(typedResponseBody.user).toStrictEqual(existingGuestUser);
       });
     });
   });
