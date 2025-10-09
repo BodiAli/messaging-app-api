157d6d9b4f44c69961882be106f93e2070275cae
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-10-09 23:06:13 +0300

    Feat: Use validateBody middleware && Handle POST requests for /log-in route

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import * as authController from "../controllers/authController.js";
+import validateBody from "../middleware/validateBody.js";
+import { UserSignUpSchema, UserLogInSchema } from "../lib/zodSchemas.js";
 
 const authRouter = Router();
 
-authRouter.post("/sign-up", authController.createUser);
+authRouter.post("/sign-up", validateBody(UserSignUpSchema), authController.createUser);
+authRouter.post("/log-in", validateBody(UserLogInSchema), authController.authenticateUser);
 
 export default authRouter;
