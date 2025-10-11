33e24db573a5bf0914e4005cb7ed3982813acf7a
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-10-11 22:09:48 +0300

    Feat: Create query for getting user notifications

---
+++ src/models/notificationModel.ts
@@ -0,0 +1,42 @@
+import prisma from "./prisma.js";
+
+export async function getUserNotifications(id: string) {
+  const notifications = await prisma.notification.findMany({
+    where: {
+      userId: id,
+
+      friendRequest: {
+        status: "PENDING",
+      },
+    },
+
+    include: {
+      friendRequest: {
+        include: {
+          sender: {
+            omit: {
+              password: true,
+              lastSeen: true,
+              isGuest: true,
+            },
+          },
+        },
+        omit: {
+          senderId: true,
+        },
+      },
+      groupChatInvitation: {
+        include: {
+          admin: true,
+        },
+      },
+    },
+    omit: {
+      friendRequestId: true,
+      groupChatInvitationId: true,
+      userId: true,
+    },
+  });
+
+  return notifications;
+}
