b7155881b208c8549de59cf0e4fd20b261429472
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-10-11 22:09:11 +0300

    Feat: Create queries for friendship logic

---
+++ src/models/friendshipModel.ts
@@ -0,0 +1,88 @@
+import prisma from "./prisma.js";
+
+export async function sendFriendRequest(senderId: string, receiverId: string) {
+  const friendship = await prisma.friendship.create({
+    data: {
+      notification: {
+        create: {
+          type: "FRIEND_REQUEST",
+          userId: receiverId,
+        },
+      },
+      senderId,
+      receiverId: receiverId,
+      status: "PENDING",
+    },
+  });
+
+  return friendship;
+}
+
+export async function acceptFriendRequest(friendRequestId: string) {
+  const friendship = await prisma.friendship.update({
+    where: {
+      id: friendRequestId,
+    },
+    data: {
+      status: "ACCEPTED",
+    },
+  });
+
+  return friendship;
+}
+
+export async function getUserFriends(id: string) {
+  const user = await prisma.user.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      receivedFriendRequests: {
+        where: {
+          status: "ACCEPTED",
+        },
+        select: {
+          sender: {
+            omit: {
+              isGuest: true,
+              password: true,
+            },
+          },
+        },
+      },
+      sentFriendRequests: {
+        where: {
+          status: "ACCEPTED",
+        },
+        include: {
+          receiver: {
+            omit: {
+              isGuest: true,
+              password: true,
+            },
+          },
+        },
+      },
+    },
+
+    omit: {
+      password: true,
+    },
+  });
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  const acceptedFriendRequests = user.receivedFriendRequests.map((request) => {
+    return request.sender;
+  });
+
+  const acceptedSentRequests = user.sentFriendRequests.map((request) => {
+    return request.receiver;
+  });
+
+  const friends = [...acceptedFriendRequests, ...acceptedSentRequests];
+
+  return friends;
+}
