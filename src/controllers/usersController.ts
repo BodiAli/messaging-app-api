b40b80441ff15ff6b7c04f73bc2b9a9900dd99ae
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-10-13 01:29:53 +0300

    Feat: Create controller to get user's friends

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,3 +1,12 @@
 import type { Request, Response } from "express";
+import * as friendshipModel from "../models/friendshipModel.js";
 
-export async function getUserFriends(req: Request, res: Response) {}
+export async function getUserFriends(req: Request, res: Response) {
+  if (!req.user) {
+    throw new Error("User not found");
+  }
+
+  const friends = await friendshipModel.getUserFriends(req.user.id);
+
+  res.status(200).json({ friends });
+}
