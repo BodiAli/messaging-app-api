f5e132d7d979667e7e28d0743d65f28e1555e0d7
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-10-12 04:10:56 +0300

    Feat: Handle case if a friend request is already sent

--- src/controllers/friendshipsController.ts
+++ src/controllers/friendshipsController.ts
@@ -1,5 +1,23 @@
 import type { Request, Response } from "express";
+import * as friendshipModel from "../models/friendshipModel.js";
 
-export async function createFriendRequest(req: Request, res: Response) {
-  res.json({ success: true });
+export async function createFriendRequest(
+  req: Request<object, object, { senderId: string; receiverId: string }>,
+  res: Response
+) {
+  const { receiverId, senderId } = req.body;
+
+  const existingFriendRequest = await friendshipModel.getFriendRequestRecord(senderId, receiverId);
+
+  if (existingFriendRequest) {
+    res
+      .status(409)
+      .json({ errors: [`A friend request is already sent by ${existingFriendRequest.sender.username}`] });
+
+    return;
+  }
+
+  // await friendshipModel.sendFriendRequest();
+
+  res.status(201).json({ message: "Friend request sent to bodi" });
 }
