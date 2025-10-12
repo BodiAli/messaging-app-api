8abfb96f78ddafa3f4ee3235832c82e6db7f4bb2
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-10-13 01:07:36 +0300

    Feat: Create routes to handle rejecting and accepting friend requests

--- src/routes/friendshipsRouter.ts
+++ src/routes/friendshipsRouter.ts
@@ -8,4 +8,7 @@ friendshipsRouter.use(passport.authenticate("jwt", { session: false }));
 
 friendshipsRouter.post("/", friendshipsController.createFriendRequest);
 
+friendshipsRouter.patch("/:id", friendshipsController.updateFriendRequest);
+friendshipsRouter.delete("/:id", friendshipsController.deleteFriendRequest);
+
 export default friendshipsRouter;
