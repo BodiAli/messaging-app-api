f8e47a06cb56b22fe07849e92ed49f263016be51
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-10-13 05:24:40 +0300

    Feat: Create tests for getting current user notifications

---
+++ src/tests/integration/notificationsRouter.test.ts
@@ -0,0 +1,125 @@
+import { describe, it, expect, beforeEach } from "vitest";
+import express from "express";
+import request from "supertest";
+import type { Friendship, Notification, User } from "../../generated/prisma/index.js";
+import notificationsRouter from "../../routes/notificationsRouter.js";
+import * as userModel from "../../models/userModel.js";
+import * as friendshipModel from "../../models/friendshipModel.js";
+import issueJwt from "../../lib/issueJwt.js";
+import "../../config/passportConfig.js";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/notifications", notificationsRouter);
+
+interface NotificationsResponse {
+  notifications: (Omit<Notification, "groupChatId" | "friendRequestId" | "userId"> & {
+    friendRequest: Omit<Friendship, "senderId"> & {
+      sender: Omit<User, "password" | "lastSeen" | "isGuest">;
+    };
+  })[];
+}
+
+describe("notificationsRouter routes", () => {
+  describe("get logged in user's notifications GET /notifications/me", () => {
+    describe("given sent friend requests to userA", () => {
+      let userA: Omit<User, "password">;
+      let userB: Omit<User, "password">;
+      let userC: Omit<User, "password">;
+
+      beforeEach(async () => {
+        userA = await userModel.createUserRecord("userA", "12345");
+        userB = await userModel.createUserRecord("userB", "12345");
+        userC = await userModel.createUserRecord("userC", "12345");
+
+        await friendshipModel.sendFriendRequest(userB.id, userA.id);
+        await friendshipModel.sendFriendRequest(userC.id, userA.id);
+      });
+
+      it("should return 200 status with userA's notifications", async () => {
+        expect.hasAssertions();
+
+        const userAToken = issueJwt(userA.id, "10m");
+
+        const response = await request(app)
+          .get("/notifications/me")
+          .auth(userAToken, { type: "bearer" })
+          .expect("Content-type", /json/)
+          .expect(200);
+
+        const typedResponseBody = response.body as NotificationsResponse;
+
+        expect(typedResponseBody.notifications).toStrictEqual([
+          {
+            id: expect.any(String) as string,
+            createdAt: expect.any(String) as string,
+            type: "FRIEND_REQUEST",
+            groupChatInvitation: null,
+            friendRequest: {
+              id: expect.any(String) as string,
+              receiverId: userA.id,
+              status: "PENDING",
+              createdAt: expect.any(String) as string,
+              sender: {
+                id: userB.id,
+                username: "userB",
+                imageUrl: null,
+              },
+            },
+          },
+          {
+            id: expect.any(String) as string,
+            createdAt: expect.any(String) as string,
+            type: "FRIEND_REQUEST",
+            groupChatInvitation: null,
+            friendRequest: {
+              id: expect.any(String) as string,
+              receiverId: userA.id,
+              status: "PENDING",
+              createdAt: expect.any(String) as string,
+              sender: {
+                id: userC.id,
+                username: "userC",
+                imageUrl: null,
+              },
+            },
+          },
+        ]);
+      });
+
+      it("should return 200 status with userB's empty notifications", async () => {
+        expect.hasAssertions();
+
+        const userBToken = issueJwt(userB.id, "10m");
+
+        const response = await request(app)
+          .get("/notifications/me")
+          .auth(userBToken, { type: "bearer" })
+          .expect("Content-type", /json/)
+          .expect(200);
+
+        const typedResponseBody = response.body as { notifications: never[] };
+
+        expect(typedResponseBody.notifications).toHaveLength(0);
+      });
+
+      it("should return 200 status with userC's empty notifications", async () => {
+        expect.hasAssertions();
+
+        const userCToken = issueJwt(userC.id, "10m");
+
+        const response = await request(app)
+          .get("/notifications/me")
+          .auth(userCToken, { type: "bearer" })
+          .expect("Content-type", /json/)
+          .expect(200);
+
+        const typedResponseBody = response.body as { notifications: never[] };
+
+        expect(typedResponseBody.notifications).toHaveLength(0);
+      });
+    });
+  });
+});
