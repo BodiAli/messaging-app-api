c98e669ef1586520a832fd525e7251402d8201d4
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-10-14 22:42:44 +0300

    Feat: create controller to be able to get the chat of two users

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,5 +1,6 @@
 import type { Request, Response } from "express";
 import * as friendshipModel from "../models/friendshipModel.js";
+import * as messageModel from "../models/messageModel.js";
 
 export async function getUserFriends(req: Request, res: Response) {
   if (!req.user) {
@@ -9,4 +10,16 @@ export async function getUserFriends(req: Request, res: Response) {
   const friends = await friendshipModel.getUserFriends(req.user.id);
 
   res.status(200).json({ friends });
+}
+
+export async function getTwoUsersMessages(req: Request<{ id: string }>, res: Response) {
+  if (!req.user) {
+    throw new Error("User not found");
+  }
+
+  const { id } = req.params;
+
+  const messages = await messageModel.getMessagesBetweenTwoUsers(req.user.id, id);
+
+  res.json({ messages });
 }
