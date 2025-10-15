1c5dd5f01f92d7f283b9168ce5f44c2ada74713d
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-10-16 02:30:22 +0300

    Feat: Allow user to send messages while handling invalid param

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,6 +1,7 @@
 import type { Request, Response } from "express";
 import * as friendshipModel from "../models/friendshipModel.js";
 import * as messageModel from "../models/messageModel.js";
+import * as userModel from "../models/userModel.js";
 
 export async function getUserFriends(req: Request, res: Response) {
   if (!req.user) {
@@ -22,4 +23,31 @@ export async function getTwoUsersMessages(req: Request<{ id: string }>, res: Res
   const messages = await messageModel.getMessagesBetweenTwoUsers(req.user.id, id);
 
   res.json({ messages });
+}
+
+export async function createMessage(
+  req: Request<{ id: string }, object, { messageContent: string }>,
+  res: Response
+) {
+  if (!req.user) {
+    throw new Error("User not found");
+  }
+
+  const { id } = req.params;
+
+  const doesUserExist = await userModel.getUserRecordById(id);
+
+  if (!doesUserExist) {
+    res.status(404).json({ errors: [{ message: "Cannot find user to send message to." }] });
+    return;
+  }
+
+  const { messageContent } = req.body;
+
+  await messageModel.sendMessageFromUserToUser(req.user.id, id, {
+    content: messageContent,
+    imageUrl: null,
+  });
+
+  res.sendStatus(201);
 }
