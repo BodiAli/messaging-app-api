9ff3cafee5d734fcd4aa389c385058664362aecc
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-10-18 03:05:39 +0300

    Feat: User multer to process multipart/formdata request and validate body + req.file

--- src/routes/usersRouter.ts
+++ src/routes/usersRouter.ts
@@ -1,6 +1,10 @@
 import { Router } from "express";
 import passport from "passport";
+import upload from "../config/multerConfig.js";
+import validateBody from "../middlewares/validateBody.js";
+import validateFile from "../middlewares/validateFile.js";
 import * as usersController from "../controllers/usersController.js";
+import { FileSchema, MessageContentSchema } from "../lib/zodSchemas.js";
 
 const usersRouter = Router();
 
@@ -9,6 +13,12 @@ usersRouter.use(passport.authenticate("jwt", { session: false }));
 usersRouter.get("/me/friends", usersController.getUserFriends);
 usersRouter.get("/:id/messages", usersController.getTwoUsersMessages);
 
-usersRouter.post("/:id/messages", usersController.createMessage);
+usersRouter.post(
+  "/:id/messages",
+  upload.single("messageImage"),
+  validateFile(FileSchema),
+  validateBody(MessageContentSchema),
+  usersController.createMessage
+);
 
 export default usersRouter;
