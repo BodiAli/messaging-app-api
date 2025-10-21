ac0e35dee2442f9255c43ac6008a326b4c6d0105
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-10-21 11:47:26 +0300

    Feat: Handle expected requests
    Create routes to handle creating a new group
    and a route to handle getting group details

--- src/routes/groupsRouter.ts
+++ src/routes/groupsRouter.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
+import validateBody from "../middlewares/validateBody.js";
+import { GroupSchema } from "../lib/zodSchemas.js";
 import * as groupsController from "../controllers/groupsController.js";
 
 const groupsRouter = Router({ mergeParams: true });
 
 groupsRouter.get("/", groupsController.getUserGroups);
+
+groupsRouter.post("/", validateBody(GroupSchema), groupsController.createGroup);
+
+groupsRouter.get("/:groupId", groupsController.getGroupWithMembers);
 
 export default groupsRouter;
