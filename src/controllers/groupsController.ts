daee17b458217db1cd5148d341c49411e5b471fa
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-10-21 11:43:24 +0300

    Feat: Create controllers for handling expected requests

--- src/controllers/groupsController.ts
+++ src/controllers/groupsController.ts
@@ -1,9 +1,46 @@
 import type { Request, Response } from "express";
+import * as groupModel from "../models/groupModel.js";
 
-export async function getUserGroups(req: Request<{ userId: string }>, res: Response) {
-  const { userId } = req.params;
+export async function getUserGroups(req: Request, res: Response) {
+  if (!req.user) {
+    throw new Error("User not found");
+  }
 
-  // const userGroups = await groupModel.
+  const userGroups = await groupModel.getUserGroups(req.user.id);
 
-  res.json("hiii");
+  res.json({ groups: userGroups });
+}
+
+export async function createGroup(req: Request<object, object, { groupName: string }>, res: Response) {
+  if (!req.user) {
+    throw new Error("User not found");
+  }
+
+  const { groupName } = req.body;
+
+  const createdGroup = await groupModel.createGroup(groupName, req.user.id);
+
+  res.status(201).json({ group: createdGroup });
+}
+
+export async function getGroupWithMembers(req: Request<{ groupId: string }>, res: Response) {
+  if (!req.user) {
+    throw new Error("User not found");
+  }
+
+  const { groupId } = req.params;
+
+  const groupWithMembers = await groupModel.getGroupWithMembers(groupId);
+
+  if (!groupWithMembers) {
+    res.status(404).json({
+      errors: [
+        { message: "Post not found! it may have been moved, deleted or it might have never existed." },
+      ],
+    });
+
+    return;
+  }
+
+  res.status(404).json({ group: groupWithMembers });
 }
