5f89121307b5460650180f437aad424a732b93f8
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-10-23 08:40:25 +0300

    Feat: Handle requests to accept/reject group invites and update group name

--- src/routes/groupsRouter.ts
+++ src/routes/groupsRouter.ts
@@ -6,15 +6,17 @@ import * as groupsController from "../controllers/groupsController.js";
 const groupsRouter = Router({ mergeParams: true });
 
 groupsRouter.get("/", groupsController.getUserGroups);
-
 groupsRouter.post("/", validateBody(GroupSchema), groupsController.createGroup);
 
 groupsRouter.get("/:groupId", groupsController.getGroupWithMembers);
+groupsRouter.patch("/:groupId", validateBody(GroupSchema), groupsController.updateGroupName);
 
 groupsRouter.post(
   "/:groupId/notifications",
   validateBody(SendGroupInviteToUsers),
   groupsController.createGroupInvite
 );
+groupsRouter.delete("/:groupId/notifications", groupsController.deleteGroupInvite);
+groupsRouter.patch("/:groupId/notifications", groupsController.acceptGroupInvite);
 
 export default groupsRouter;
