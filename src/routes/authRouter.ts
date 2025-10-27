8ad4ac975b57429170ea30cc4f948fbbbdfa1c9c
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-10-27 09:17:03 +0300

    Feat: Handle requests to get current user

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,11 +1,17 @@
-import { Router } from "express";
+import { Router, type RequestHandler } from "express";
 import * as authController from "../controllers/authController.js";
 import validateBody from "../middlewares/validateBody.js";
 import { UserSignUpSchema, UserLogInSchema } from "../lib/zodSchemas.js";
+import passport from "passport";
 
 const authRouter = Router();
 
 authRouter.get("/guest", authController.logInAsGuest);
+authRouter.get(
+  "/get-user",
+  passport.authenticate("jwt", { session: false }) as RequestHandler,
+  authController.getUser
+);
 authRouter.post("/sign-up", validateBody(UserSignUpSchema), authController.createUser);
 authRouter.post("/log-in", validateBody(UserLogInSchema), authController.authenticateUser);
 
