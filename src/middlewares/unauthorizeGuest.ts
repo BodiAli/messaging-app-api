ee83d57e40baa9729f621cbddb8d0c3ae349fc98
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-10-29 08:45:01 +0300

    Feat: Create middleware to unauthorize guest users from accessing specific endpoints

---
+++ src/middlewares/unauthorizeGuest.ts
@@ -0,0 +1,13 @@
+import type { NextFunction, Request, Response } from "express";
+
+export default function unauthorizeGuest(req: Request, res: Response, next: NextFunction) {
+  if (!req.user) {
+    throw new Error("User not found");
+  }
+
+  if (req.user.isGuest) {
+    res.status(403).json({ errors: [{ message: "You must have an account to complete this request." }] });
+  }
+
+  next();
+}
